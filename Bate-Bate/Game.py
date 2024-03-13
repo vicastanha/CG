bc57bb2851f033acf1f9b4a13d588b7bda443255
vicastanha <vicastanha10@gmail.com>
2024-03-13 20:13:45 -0300

    aula 13-03-2024 (batebate)

--- Bate-Bate/Game.py
+++ Bate-Bate/Game.py
@@ -1,29 +1,31 @@
+
 import pygame
 import sys
-from  MecMovimento import MovendoTexto
+from MecMovimento import MovendoTexto
+
 
 class Game:
     def __init__(self):
         pygame.init()
         self.largura = 800
-        self.altura=600
-        self.tela = pygame.display.set.mode((self.largura, self.altura, self.tela))
-        pygame.display.set.caption("Bate-Bate")
+        self.altura = 600
+        self.tela = pygame.display.set_mode((self.largura, self.altura))
+        pygame.display.set_caption("Bate-Bate")
         self.clock = pygame.time.Clock()
-        self.MovendoTexto = MovendoTexto("Vitoria",50, self.largura, self.altura)
-    
-    def run (self):
-        rodando=True
+        self.MovendoTexto = MovendoTexto("Vitória", 50, self.largura, self.altura)
+
+    def run(self):
+        rodando = True
         while rodando:
             for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
                     rodando = False
-                    
-                self.MovendoTexto.move()
-                self.tela.fill((0,0,0))
-                self.tela.blit(self.MovendoTexto.texto_surf, self.MovendoTexto.texto_rect)
-                pygame.display.flip()
-                self.clock.tick(60)
-                
+
+            self.MovendoTexto.move()
+            self.tela.fill((0, 0, 0))
+            self.tela.blit(self.MovendoTexto.texto_surf, self.MovendoTexto.rect)
+            pygame.display.flip()
+            self.clock.tick(60)
+
         pygame.quit()
         sys.exit()
